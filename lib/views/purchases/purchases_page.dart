c8ab38d60c9a7189456d69eeceba7be9430a1140
Dagmawi Yohannes <dagmawiyohannes02@gmail.com>
2021-08-15 15:11:36 +0300

    purchases_page Update

--- lib/views/purchases/purchases_page.dart
+++ lib/views/purchases/purchases_page.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:eshemachoch_mobile_application/views/purchases/qrcode_page.dart';
 
 class MyPurchasesPage extends StatelessWidget {
   const MyPurchasesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Item $index'),
-          subtitle: Text('DateTime'),
-          trailing: Text('CLAIMED'),
-        );
-      },
-    );
+    Widget _buildClaimButton() {
+      return ElevatedButton(
+        child: Text('CLAIM'),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return MyQRCodePage();
+          }));
+        },
+      );
+    }
   }
 }
