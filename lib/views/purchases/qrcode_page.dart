945e64723714f6fa90879af73f203f5e5802310f
Dagmawi Yohannes <dagmawiyohannes02@gmail.com>
2021-08-15 15:21:07 +0300

    qrcode_page Update

--- lib/views/purchases/qrcode_page.dart
+++ lib/views/purchases/qrcode_page.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class MyQRCodePage extends StatelessWidget {
+  const MyQRCodePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(56),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(height: 200, color: Colors.blueGrey),
+            SizedBox(height: 24.0),
+            Text(
+                'Show this QR Code to claim your purchased items from a nearby E-Shemachoch Store.')
+          ],
+        ),
+      ),
+    );
+  }
+}
