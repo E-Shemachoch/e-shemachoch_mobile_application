1bceb02db82da2b4bd58b7b5a1415a920d8d33e1
lotiber <56714084+lotiber@users.noreply.github.com>
2021-08-17 12:58:12 -0400

    Update home_page.dart

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:eshemachoch_mobile_application/views/home/widgets/product_tile.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -6,11 +7,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      padding: EdgeInsets.all(10.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
       ),
       itemBuilder: (BuildContext context, int index) {
-        return Placeholder();
+        return MyProductTile();
       },
       itemCount: 25,
     );
