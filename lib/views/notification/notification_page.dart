025f2cbedee7c644487cc8f7b3070d8f793d29a7
GeruMan1 <83118960+GeruMan1@users.noreply.github.com>
2021-09-14 13:45:49 +0300

    Update notification_page.dart
    update

--- lib/views/notification/notification_page.dart
+++ lib/views/notification/notification_page.dart
@@ -1,21 +1,30 @@
+import 'package:eshemachoch_mobile_application/viewmodels/notice/notice_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyNotificationPage extends StatelessWidget {
   const MyNotificationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: FlutterLogo(),
-          title: Text('Notification $index'),
-          subtitle: Text('DateTime'),
+    return Consumer<NoticeModel>(
+      builder: (context, model, child) {
+        if (model.hasError) return Text('Failed to load.');
+        if (model.isLoading) return CircularProgressIndicator();
+        return ListView.separated(
+          itemBuilder: (context, index) {
+            final notice = model.notices![index];
+            final date = DateTime.fromMillisecondsSinceEpoch(notice.date);
+            return ListTile(
+              title: Text(notice.title),
+              subtitle: Text(date.toString()),
+            );
+          },
+          itemCount: model.notices!.length,
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider();
+          },
         );
-      },
-      itemCount: 4,
-      separatorBuilder: (BuildContext context, int index) {
-        return Divider();
       },
     );
   }
