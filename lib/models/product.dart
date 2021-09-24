8d4a5983efe67d4fbb3fc3fa24b7407de8df6bdb
lotiber <lotiberhanu1@gmail.com>
2021-09-24 09:12:05 -0400

    product model

---
+++ lib/models/product.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
+class Product {
+  Product(this.id, this.name, this.price, this.quantity, this.image, this.date);
+
+  final String? id;
+  final String name;
+  final double price;
+  final int quantity;
+  final String image;
+  final int date;
+
+  factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
+
+  Product copyWith(
+          {String? id,
+          String? name,
+          double? price,
+          int? quantity,
+          String? image,
+          int? date}) =>
+      Product(
+        id,
+        name ?? this.name,
+        price ?? this.price,
+        quantity ?? this.quantity,
+        image ?? this.image,
+        date ?? this.date,
+      );
+  static Product empty() => Product('', '', 0, 0, '', 0);
+}
---
+++ lib/models/product.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Product _$ProductFromJson(Map<String, dynamic> json) => Product(
+      json['id'] as String?,
+      json['name'] as String,
+      (json['price'] as num).toDouble(),
+      json['quantity'] as int,
+      json['image'] as String,
+      json['date'] as int,
+    );
+
+Map<String, dynamic> _$ProductToJson(Product instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  val['name'] = instance.name;
+  val['price'] = instance.price;
+  val['quantity'] = instance.quantity;
+  val['image'] = instance.image;
+  val['date'] = instance.date;
+  return val;
+}
